e4c661f1461a0aeabbf1a3cdae52aabadcfdf073
sanggeun12 <mio0124kio@gmail.com>
2022-04-04 13:09:05 +0900

    new file

---
+++ 4.py
@@ -0,0 +1,12 @@
+scr = int (input ())
+
+if scr >= 90 and scr <= 100 :
+    print("A")
+elif scr >= 80 :
+    print ("B")
+elif scr >= 70 :
+    print ("C")
+elif scr >= 60 :
+    print ("D")
+else :
+    print ("F")
